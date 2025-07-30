cc7f89d57e39fe5fc4d791077f54cbea76b33bc1
Ayan Das <ayan@anjandas.net>
2025-07-29 21:26:30 -0400

    pushing terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket  = "terrforms3-remote-backend"
+    bucket  = "terraform-backend-396008559107"
     key     = "global/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
